50d6f180cfb3d32fe5fabcabe1487e21c32715cd
peterxcli <peterxcli@gmail.com>
2024-09-25 19:51:55 +0800

    refactor: Improve loop logic and comments in lab02.X/lab2-1.asm

--- lab02.X/lab2-1.asm
+++ lab02.X/lab2-1.asm
@@ -16,28 +16,27 @@ setup1:
     LFSR 0, 0x100 ; FSR0 points to 0x100
     LFSR 1, 0x116 ; FSR1 points to 0x116
 
-loop1:
-    ; Check if INDF0 equals 0x106
-    MOVLW 0x106
-    CPFSEQ INDF0
-    GOTO continue_loop
-    GOTO loop1_end
+    ; Initialize loop counter
+    MOVLW 0x06
+    MOVWF 0x20 ; Use 0x20 as loop counter
 
-continue_loop:
+loop1:
     ; Set [INDF0 + 1] = [INDF0] + [INDF1]
-    MOVF POSTINC0, W ; Load [INDF0] into W and increment FSR0
+    MOVFF POSTINC0, W ; Load [INDF0] into W and increment FSR0
     ADDWF INDF1, W ; Add [INDF1] to W
-    MOVWF POSTINC0 ; Store result in [INDF0] and increment FSR0
+    MOVFF W, INDF0 ; Store result in [INDF0] and increment FSR0
 
     ; INDF0++ (already done by POSTINC0 above)
 
     ; Set [INDF1] = [INDF0] + [INDF1]
-    MOVF INDF0, W ; Load [INDF0] into W
+    MOVFF INDF0, W ; Load [INDF0] into W
     ADDWF POSTDEC1, W ; Add [INDF1] to W
-    MOVWF INDF1 ; Store result in [INDF1]
+    MOVFF W, INDF1 ; Store result in [INDF1]
     
     ; INDF1-- (already done by POSTDEC1 above)
 
+    ; Decrement loop counter
+    DECFSZ 0x20,F
     GOTO loop1
 
 loop1_end:
