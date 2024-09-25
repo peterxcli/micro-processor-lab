59d1de6cfd5d6898e525c52b86f9af2a0a264133
peterxcli <peterxcli@gmail.com>
2024-09-25 20:31:07 +0800

    refactor: Improve loop logic and comments in lab02.X/lab2-1.asm

--- lab02.X/lab2-1.asm
+++ lab02.X/lab2-1.asm
@@ -5,40 +5,41 @@ List p=18f4520
     org 0x00 
 
 setup1:
+    ; Set Bank 1
+    MOVLB 0x1
+
     ; Store 0x00 in [0x100]
     MOVLW 0x00
-    MOVWF 0x100
+    MOVWF 0x00, 1
 
     ; Store 0x01 in [0x116]
     MOVLW 0x01
-    MOVWF 0x116
+    MOVWF 0x16, 1
 
     LFSR 0, 0x100 ; FSR0 points to 0x100
     LFSR 1, 0x116 ; FSR1 points to 0x116
 
     ; Initialize loop counter
     MOVLW 0x06
-    MOVWF 0x20 ; Use 0x20 as loop counter
+    MOVWF 0x00
 
 loop1:
     ; Set [INDF0 + 1] = [INDF0] + [INDF1]
-    MOVFF POSTINC0, W ; Load [INDF0] into W and increment FSR0
+    MOVF POSTINC0, W ; Load [INDF0] into W and increment FSR0
     ADDWF INDF1, W ; Add [INDF1] to W
-    MOVFF W, INDF0 ; Store result in [INDF0] and increment FSR0
+    MOVWF INDF0 ; Store result in [INDF0] and increment FSR0
 
     ; INDF0++ (already done by POSTINC0 above)
 
     ; Set [INDF1] = [INDF0] + [INDF1]
-    MOVFF INDF0, W ; Load [INDF0] into W
+    MOVF INDF0, W ; Load [INDF0] into W
     ADDWF POSTDEC1, W ; Add [INDF1] to W
-    MOVFF W, INDF1 ; Store result in [INDF1]
+    MOVWF INDF1 ; Store result in [INDF1]
     
     ; INDF1-- (already done by POSTDEC1 above)
 
     ; Decrement loop counter
-    DECFSZ 0x20,F
+    DECFSZ 0x000
     GOTO loop1
-
-loop1_end:
 
 end
