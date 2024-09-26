35096c12c9b023d2bcf2349f2d1dbd57acbe7e88
peterxcli <peterxcli@gmail.com>
2024-09-26 10:17:28 +0800

    refactor: Improve loop logic and comments in lab02.X/lab2-3.asm

--- lab02.X/lab2-3.asm
+++ lab02.X/lab2-3.asm
@@ -2,90 +2,79 @@ List p=18f4520
 #include<pic18f4520.inc>
     CONFIG OSC = INTIO67
     CONFIG WDT = OFF
-    org 0x00 ;PC = 0x00
-    
+    org 0x00
+
+    target equ 0x23
+    ans equ 0x011
+
 setup:
-    MOVLW 0xFE
-    MOVWF 0x09
-    
-    MOVLW 0x00
-    MOVWF 0x11
-    
-    MOVLW 0x7F
-    MOVWF 0x16
-    
     LFSR 0, 0x000
+
+    MOVLW 0x28
+    MOVWF POSTINC0
+    MOVLW 0x34
+    MOVWF POSTINC0
+    MOVLW 0x7A
+    MOVWF POSTINC0
+    MOVLW 0x80
+    MOVWF POSTINC0
+    MOVLW 0xA7
+    MOVWF POSTINC0
+    MOVLW 0xD1
+    MOVWF POSTINC0
+    MOVLW 0xFE
+    MOVWF POSTINC0
     
     MOVLW 0x28
-    MOVWF POSTINC0 
-    MOVLW 0x34
-    MOVWF POSTINC0 
-    MOVLW 0x7A
-    MOVWF POSTINC0 
-    MOVLW 0x80
-    MOVWF POSTINC0 
-    MOVLW 0xA7
-    MOVWF POSTINC0 
-    MOVLW 0xD1
-    MOVWF POSTINC0 
-    MOVLW 0xFE
-    MOVWF POSTINC0 
+    MOVWF target
     
-    MOVLW 0x10
-    
-    LFSR 0, 0x000; l
-    LFSR 1, 0x006; r
-    LFSR 2, 0x003; mid
+    LFSR 0, 0x000 // left = 0
+    LFSR 1, 0x006 // right = 6
 
+while_loop:
+    MOVF FSR1, W // while(left < right)
+    CPFSLT FSR0 // F < W (left < right)
+    GOTO end_while
     
-main:
-    MOVF FSR0L,W
-    CPFSLT FSR1L
-    GOTO checker
-    GOTO finish
+    MOVF FSR0, W // W = left
+    ADDWF FSR1,W // W = left + right
+    RRCF WREG, W // W = (left + right) / 2
+    MOVWF FSR2   // mid = W
     
+    /*
+	if(mid < target) l = mid + 1;
+	else r = mid
+    */
+    MOVF target, W // W = target
+    CPFSLT INDF2  // a[mid] < target
+    GOTO left_half 
+    GOTO right_half
+	
+right_half: 
+	MOVF FSR2, W
+    INCF INDF0 // l = mid + 1;
+	GOTO while_loop
+	
+left_half:
+	MOVF FSR2, W
+	MOVWF FSR1 // r = mid
+	GOTO while_loop
+ 
+end_while:
+    MOVF INDF0, W 
+    CPFSEQ target // if(a[l] == target)
+    GOTO not_find
+    GOTO find
+    
+find:
+	MOVLW 0xFF
+	MOVWF ans
+	GOTO finish
 
-checker:
-    MOVFf INDF2, 0x22
-    MOVF INDF2,w
-    CPFSEQ 0x09
-    GOTO not_found
-    GOTO found
-    
-    
-not_found:
-    MOVF INDF2,w
-    CPFSGT 0x09
-    GOTO left
-    GOTO right
+not_find:
+	MOVLW 0x00
+	MOVWF ans
+	GOTO finish
 
-right:
-    INCF FSR2L
-    MOVFf FSR2L,FSR0L
-    MOVF  FSR0L,W
-    addwf FSR1L,W
-    
-    GOTO divide
-    
- left:
-    DECF FSR2L
-    MOVFf FSR2L,FSR1L
-    MOVF  FSR0L,W
-    addwf FSR2L,W
-    
-    GOTO divide
-    
-divide:
-    RRNCF WREG
-    ANDWF 0x16, w
-    MOVWF 0x18
-    
-    MOVWF FSR2L
-    GOTO main
-  
-found:
-    MOVLW 0xFF 
-    MOVWF 0x11
 finish:
-    
 end
